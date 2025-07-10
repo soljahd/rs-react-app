f83b729ab0e182db7eb3513e9589d80ff2ef2879
soljah <moskuitoz@gmail.com>
2025-07-10 20:59:31 +0300

    feat: search component implemented

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,26 @@ import { Component } from 'react';
 import Search from './components/Search';
 import Results from './components/Results';
 
-class App extends Component {
+type State = {
+  searchQuery: string;
+};
+
+class App extends Component<unknown, State> {
+  state: State = {
+    searchQuery: localStorage.getItem('searchQuery') || '',
+  };
+
+  handleSearch = (query: string) => {
+    localStorage.setItem('searchQuery', query);
+    this.setState({ searchQuery: query }, () => {
+      console.log(query);
+    });
+  };
+
   render() {
     return (
       <div className="app mx-auto flex min-h-screen max-w-5xl min-w-xs flex-col justify-center gap-1 p-4">
-        <Search />
+        <Search initialValue={this.state.searchQuery} onSearch={this.handleSearch} />
         <Results />
       </div>
     );
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,16 +1,38 @@
-import { Component } from 'react';
+import { Component, type ChangeEvent } from 'react';
 
-class Search extends Component {
+type Props = {
+  initialValue: string;
+  onSearch: (qery: string) => void;
+};
+
+type State = {
+  searchQuery: string;
+};
+
+class Search extends Component<Props, State> {
+  state = { searchQuery: this.props.initialValue };
+
+  handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ searchQuery: e.target.value });
+  };
+
+  handleSubmit = () => {
+    this.props.onSearch(this.state.searchQuery.trim());
+  };
+
   render() {
     return (
       <div className="search flex gap-4">
         <input
           type="text"
           className="w-full rounded-lg border border-gray-300 bg-gray-50 p-2 focus:border-blue-700 focus:outline-none"
+          value={this.state.searchQuery}
+          onChange={this.handleChange}
         />
         <button
           type="button"
           className="min-w-24 rounded-lg bg-blue-700 px-4 py-2 font-medium text-white hover:bg-blue-800 active:bg-blue-700"
+          onClick={this.handleSubmit}
         >
           Search
         </button>
